6c71bbfaeb385ffa7cda58fd5263f65f1ebbda99
deVamshi <dev.vamshidhar@gmail.com>
2023-12-25 16:14:59 +0530

    barebones

---
+++ data/projectsData.ts
@@ -0,0 +1,20 @@
+const projectsData = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
+  {
+    title: 'Civic Issues Flutter Project',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/mobile.jpg',
+    href: '/blog/projects-info/civic_issues',
+  },
+]
+
+export default projectsData
